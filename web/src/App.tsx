5bb80fd4f8eeb194b1056261680138be272da0b2
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-17 15:37:24 -0300

    Landing Styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,11 @@
 import React from 'react';
 import './styles/global.css'
-
-interface TitleProps {
-  text: String;
-}
-
-function Title(props: TitleProps) {
-  return (
-  <h1>{props.text}</h1>
-  )
-}
+import './styles/pages/landing.css'
 
 function App() {
   return (
-    <div className="App">
-      <Title text="Oi"></Title>
+    <div id="page-landing">
+      
     </div>
   );
 }
