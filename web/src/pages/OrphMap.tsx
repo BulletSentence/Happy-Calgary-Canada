5e540de90e971f1b1ea4dd4ebccdd485ac186bce
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-17 17:06:41 -0300

    Menu lateral do mapa

--- web/src/pages/OrphMap.tsx
+++ web/src/pages/OrphMap.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import mapMarker from '../images/marker.svg'
+import { FiPlus } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import '../styles/pages/orphMap.css'
 
 function OrphMap() {
     return (
-        <h1>
-            Hello World
-        </h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarker} alt='Happy'/>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong> Calgary </strong>
+                    <span> Canada </span>
+                </footer>
+            </aside>
+            <div></div>
+            <Link to="" className="create-orph">
+                <FiPlus size={32} color="FFF"></FiPlus>
+            </Link>
+        </div>
     )
 }
 
