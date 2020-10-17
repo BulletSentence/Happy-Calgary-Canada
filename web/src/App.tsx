2fc8d9183bf1ca885c5fff7a4ed73e719472698f
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-17 15:55:48 -0300

    Estilization

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import './styles/global.css'
 import './styles/pages/landing.css'
+import logoImg from './images/Logo.svg'
 
 function App() {
   return (
     <div id="page-landing">
-      
+      <div className="content-wrapper">
+          <img src={logoImg} alt=""/>
+
+          <main>
+            <h1>Bring happiness to the world</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+          </main>
+
+          <div className="location">
+            <strong> Calgary </strong>
+            <span> Canada </span>
+          </div>
+
+          <a href='#' className='enter-app'>
+           O
+          </a>
+      </div>
     </div>
   );
 }
