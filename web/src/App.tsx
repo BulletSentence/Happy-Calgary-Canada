0490f33c9ef6a453912c112457a27afef06b2d55
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-17 15:12:13 -0300

    Salvo, texto

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+interface TitleProps {
+  text: String;
+}
+
+function Title(props: TitleProps) {
+  return (
+  <h1>{props.text}</h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title text="Oi"></Title>
     </div>
   );
 }
