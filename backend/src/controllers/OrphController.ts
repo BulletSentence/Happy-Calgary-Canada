335c2b6c80fe7ee04b6c3f470b3f9608f1517877
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-19 14:40:17 -0300

    API

--- backend/src/controllers/OrphController.ts
+++ backend/src/controllers/OrphController.ts
@@ -2,6 +2,7 @@ import {Request, Response} from 'express'
 import { getRepository } from 'typeorm';
 import Orph from '../database/models/Orph';
 import OrphView from '../views/orph_view';
+import * as Yup from 'yup';
 
 export default {
 
@@ -39,7 +40,8 @@ export default {
         const images = requestImages.map(image => {
             return { path: image.filename }
         });
-        const orph = orphRepository.create({
+
+        const data = {
             name,
             latitude,
             longitude,
@@ -48,7 +50,26 @@ export default {
             opening_hours,
             open_on_weekends,
             images,
+        };
+    
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required(),
+            }))
         });
+
+        await schema.validate(data, {
+            abortEarly: false,
+        });
+
+        const orph = orphRepository.create(data);
     
         await orphRepository.save(orph);
     
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import routes from './routes'
 import express from 'express';
+import 'express-async-errors';
 import './database/connection';
 import path from 'path'
+import errorHandler from './errors/handler'
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
 app.listen(3333);
