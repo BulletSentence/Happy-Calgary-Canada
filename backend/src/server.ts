420484be2c42a9c6fd33067de478ba09dd6e036f
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-19 14:42:56 -0300

    Cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import routes from './routes'
 import express from 'express';
+import path from 'path'
+import cors from 'cors';
 import 'express-async-errors';
 import './database/connection';
-import path from 'path'
 import errorHandler from './errors/handler'
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
