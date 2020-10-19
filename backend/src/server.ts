8ffe7b6fd2a31ef7e4e9e41f1bdef52e7a56c46a
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-19 07:27:56 -0300

    new

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
-import express from 'express'
+import express from 'express';
+import './database/connection';
 
 const app = express();
+app.use(express.json);
 
 app.get('/users', (request, response) => {
-
+    return response.json({message: "Oie"});
 });
 
 app.listen(3333);
