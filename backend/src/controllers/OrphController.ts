6af183c03a1e92337d387c7bf790e93fcdd3e64b
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-19 11:40:08 -0300

    Codigo abstraido

---
+++ backend/src/controllers/OrphController.ts
@@ -0,0 +1,33 @@
+import {Request, Response} from 'express'
+import { getRepository } from 'typeorm';
+import Orph from '../database/models/Orph';
+
+export default {
+    async create(request: Request, response: Response) {
+        const {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+        } = request.body;
+    
+        const orphRepository = getRepository(Orph);
+        const orph = orphRepository.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+        });
+    
+        await orphRepository.save(orph);
+    
+        console.log(request.body);
+        return response.status(201).json(orph);
+    }
+}
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,37 +1,8 @@
-
-import { Router } from 'express'
-import { getRepository } from 'typeorm';
-import Orph from './database/models/Orph';
+import { Router } from 'express';
+import OrphController from './controllers/OrphController';
 
 const routes = Router();
 
-routes.post('/orph', async (request, response) => {
+routes.post('/orph', OrphController.create);
 
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    } = request.body;
-
-    const orphRepository = getRepository(Orph);
-    const orph = orphRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends,
-    });
-
-    await orphRepository.save(orph);
-
-    console.log(request.body);
-    return response.status(201).json(orph);
-});
-
-export default routes;
+export default routes; 
