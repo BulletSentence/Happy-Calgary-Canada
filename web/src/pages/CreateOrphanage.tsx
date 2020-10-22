c9523562b0f011b0e10cdb2fd53e4f7109a4361a
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-22 11:28:02 -0300

    Campo de fotos add

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -25,11 +25,12 @@ export default function CreateOrphanage() {
 
   function handleSubmit(event: FormEvent) {
     event.preventDefault();
-
+    const { latitude, longitude } = position;
     console.log({
-      position,
       name,
       about,
+      latitude,
+      longitude,
       instructions,
       opening_hours,
     })
@@ -80,13 +81,11 @@ export default function CreateOrphanage() {
             <div className="input-block">
               <label htmlFor="images">Fotos</label>
 
-              <div className="uploaded-image">
-
-              </div>
-
-              <button className="new-image">
+              <div className="images-container">
+              <button type='button' className="new-image">
                 <FiPlus size={24} color="#15b6d6" />
               </button>
+              </div>
             </div>
           </fieldset>
 
