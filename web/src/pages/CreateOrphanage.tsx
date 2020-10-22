b72a7728419607735a6826bfa4f1aa5fe6cdf54a
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-22 10:47:07 -0300

    Escolhendo lugar no mapa

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Map, Marker, TileLayer } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet'
 import { FiPlus } from "react-icons/fi";
 import '../styles/pages/create-orphanage.css';
 import Sidebar from "../components/Sidebar";
 import mapIcon from "../utils/mapIcon";
 
 export default function CreateOrphanage() {
+  
+  const [position, setPosition] = useState({latitude: 0, longitude: 0})
+
+  function handleMapClick(event: LeafletMouseEvent) {
+    const {lat, lng } = event.latlng;
+    setPosition({
+      latitude: lat,
+      longitude: lng,
+    })
+  }
 
   return (
     <div id="page-create-orphanage">
@@ -19,12 +30,17 @@ export default function CreateOrphanage() {
               center={[-27.2092052,-49.6401092]} 
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              onclick={handleMapClick}
             >
               <TileLayer 
                 url={`https://a.tile.openstreetmap.org/{z}/{x}/{y}.png`}
               />
-
-              <Marker interactive={false} icon={mapIcon} position={[-27.2092052,-49.6401092]} />
+              { position.latitude != 0 ? 
+               <Marker 
+                interactive={false} 
+                icon={mapIcon} 
+                position={[position.latitude,position.longitude]}/> 
+                : null }
             </Map>
 
             <div className="input-block">
