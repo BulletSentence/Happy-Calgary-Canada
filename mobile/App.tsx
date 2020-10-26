0b2afa3faf046bf8acbcbc4e5b578f6eb7a0cad1
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-26 13:47:58 -0300

    Ponto e texto

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,18 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions, Text } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapMarker from './src/images/map-marker-icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}
+      <MapView 
+      style={styles.map}
+      provider = {PROVIDER_GOOGLE}
       initialRegion={{
         latitude: -15.7217175,
         longitude: -48.0774442,
         latitudeDelta: 30.018,
         longitudeDelta: 30.028,
-        }}/>
+        }}>
+          <Marker 
+            icon={MapMarker}
+            coordinate={{
+              latitude: -15.7217175,
+              longitude: -48.0774442,
+            }}>
+              <Callout>
+                <Text>Ponto no Mapa</Text>
+              </Callout>
+            </Marker>
+        </MapView>
     </View>
   );
 }
