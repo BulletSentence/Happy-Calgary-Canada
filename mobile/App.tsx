8efb4f8f66df5b18899ca894d18cd9f7b2dd306c
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-10-27 09:49:24 -0300

    Rotas Ok

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { StyleSheet, View, Dimensions, Text, TouchableOpacity } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
+import Routes from './src/routes';
 
 export default function App() {
 
   return (
-  
+    <Routes/>
   );
 }
 
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import OrphMap from './pages/OrphMap';
 
 const { Navigator, Screen } = createStackNavigator();
 
-
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen name="OrphMap" component={OrphMap}/>
+            </Navigator>
+        </NavigationContainer>
+    );
+}
