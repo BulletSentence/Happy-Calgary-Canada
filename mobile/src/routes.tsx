c13eb83ae14f25f3843d3a2f44933f4aa743bb9d
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-11-03 08:44:01 -0300

    Rotas mobile

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import OrphMap from './pages/OrphMap';
 import OrphDetails from './pages/OrphDetails';
+import OrphData from './pages/CreateOrph/OrphData';
+import SelectMapPosition from './pages/CreateOrph/SelectMapPosition';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -17,6 +19,14 @@ export default function Routes() {
                  <Screen 
                     name="OrphDetails" 
                     component={OrphDetails}
+                />
+                <Screen 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition}
+                />
+                <Screen 
+                    name="OrphData" 
+                    component={OrphData}
                 />
             </Navigator>
         </NavigationContainer>
