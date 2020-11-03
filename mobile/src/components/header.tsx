3c4ec711014c8fe5aa2763ee9a2e9c928c7df02f
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-11-03 09:47:35 -0300

    Routes for buttons to GoBack and Close

--- mobile/src/components/header.tsx
+++ mobile/src/components/header.tsx
@@ -2,22 +2,29 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { Feather } from "@expo/vector-icons";
+import { useNavigation } from '@react-navigation/native';
 
 interface HeaderProps {
     title: string;
 }
 
 export default function Header(props: HeaderProps) {
+
+    const navigation = useNavigation();
+    function handleGoBack2Home() {
+        navigation.navigate('OrphMap');
+    }
+
     return (
         <View style={styles.container}>
 
-            <BorderlessButton onPress={() => {}}>
+            <BorderlessButton onPress={navigation.goBack}>
                 <Feather name="arrow-left" size={24} color='#15b6d6'/>
             </BorderlessButton>
 
             <Text style={styles.title}>{props.title}</Text>
 
-            <BorderlessButton onPress={() => {}}>
+            <BorderlessButton onPress={handleGoBack2Home}>
                 <Feather name="x" size={24} color='#ff669d'/>
             </BorderlessButton>
         </View>
